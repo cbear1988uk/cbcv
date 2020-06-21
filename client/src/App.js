9ed1fe933999d719d005be18ac830dc54e459880
Collin Bull <vhiessumaniac@hotmail.com>
2020-06-21 14:42:59 +0100

    added css and images folder

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,10 @@ function App() {
       <div id="page-wrapper">
         <div id="page-header">
           <h1>Collin Bull</h1>
+          <ul>
+            <li>07519 551240</li>
+            <li>collinjbull@gmail.com</li>
+          </ul>
           <p>Note: I have legal right to work in the UK and do not require visa sponsorship</p>
         </div>
         <div id="main">
