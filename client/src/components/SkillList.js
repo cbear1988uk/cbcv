6f3ff2c98602d5398f6cac66c94bf6112be6153e
Collin Bull <vhiessumaniac@hotmail.com>
2020-06-22 19:22:44 +0100

    added hovering effects and skills list

--- client/src/components/SkillList.js
+++ client/src/components/SkillList.js
@@ -20,9 +20,26 @@ class SkillList extends Component{
     }else{
       return data.skills.map(skill =>{
         return(
-          <div>
+          <div id="skill-content" key={skill.id}>
             <h2>{skill.title}</h2>
             <p>{skill.description}</p>
+            <div id="tags" className="grid-container">
+                <div className="grid-item">Ruby</div>
+                <div className="grid-item">JavaScript</div>
+                <div className="grid-item">Java</div>
+                <div className="grid-item">C#</div>
+                <div className="grid-item">css</div>
+                <div className="grid-item">HTML</div>
+                <div className="grid-item">node.js</div>
+                <div className="grid-item">React</div>
+                <div className="grid-item">Vue</div>
+                <div className="grid-item">MongoDB</div>
+                <div className="grid-item">SQL</div>
+                <div className="grid-item">Agile</div>
+                <div className="grid-item">Azure DevOps</div>
+                <div className="grid-item">ASP.Net</div>
+                <div className="grid-item">Selenium</div>
+            </div>
           </div>
         );
       })
